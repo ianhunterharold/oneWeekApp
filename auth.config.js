8277456ebc5f5fec2acd556fb856acb2faf7f3b0
Ian Harold <ianhunterharold@gmail.com>
2021-08-11 12:59:14 -0600

    auth set up

---
+++ auth.config.js
@@ -0,0 +1,10 @@
+export default {
+  oidc: {
+    clientId: '0oa1fx4x37ULpYtBO5d7',
+    redirectUri: 'com.okta.dev-45085254:/callback',
+    endSessionRedirectUri: 'com.okta.dev-45085254:/callback',
+    discoveryUri: 'https://dev-45085254.okta.com/oauth2/default',
+    scopes: ['openid', 'profile', 'offline_access'],
+    requireHardwareBackedKeyStore: false,
+  },
+};
